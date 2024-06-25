ef3b1d646e83466839ca442c80572ee34a85e23e
瑞池 戴 <2455708839@qq.com>
2024-06-25 18:02:43 +0800

    test

--- lightLocation.h
+++ lightLocation.h
@@ -1,17 +1,29 @@
 #pragma once
 class lightLocation {//智能家居中所有灯的属性
 private:
-	double x;//长
-	double y;//宽
-	double z;//高
+	double length;//长
+	double width;//宽
+	double height;//高
 	double watt;//功率
+	//待用
+	double maxWatt;//最大功率
+	double minWatt;//最小功率
+	double averageWatt;//平均功率
+
 public:
-	lightLocation(double x, double y, double z, double watt);
+	lightLocation(double length, double width, double height, double watt);
 	lightLocation();
-	double Get_x();
-	double Get_y();
-	double Get_z();
-	void Set_x(double x);
-	void Set_y(double y);
-	void Set_z(double z);
+	double Get_length();
+	double Get_width();
+	double Get_height();
+	double Get_watt();
+
+	double Get_maxWatt();
+	double Get_minWatt();
+	double Get_averageWatt();
+
+	void Set_length(double length);
+	void Set_width(double width);
+	void Set_height(double height);
+	void Set_watt(double watt);
 };
